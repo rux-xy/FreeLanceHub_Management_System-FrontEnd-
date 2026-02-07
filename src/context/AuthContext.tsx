0ceaaaac739590a68d220fee132914716c4d7187
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-07 13:44:27 +0530

    type1

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,3 +1,8 @@
 import React, { createContext, useEffect, useMemo, useState } from 'react';
 import axiosClient from '../lib/axios';
 import type { User } from '../types/user.types';
+
+type LoginCredentials = {
+    email: string;
+    password: string;
+  };
