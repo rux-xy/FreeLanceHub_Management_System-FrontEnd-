edbd5a7fc7b28e1bf4a68c75724b2ba7febb3c25
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-12 00:09:32 +0530

    App1

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 
+import { useAuth } from './hooks/useAuth';
+import { useEffect } from 'react';
+
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
