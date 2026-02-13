280fea7d9485ba593db1322d3403abd12753a510
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-13 16:50:19 +0530

    register 5

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,5 +5,11 @@ import { useAuth } from '../hooks/useAuth';
 
 export default function Register() {
 
-
+    const { register, isLoading, error, clearError } = useAuth();
+    const navigate = useNavigate();
+  
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [role, setRole] = useState<'client' | 'freelancer'>('freelancer');
 }
