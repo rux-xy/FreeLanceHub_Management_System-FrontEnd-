f60009ce1e879cba6d5cfc0ee4e57958e158f9a6
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 18:03:02 +0530

    job proposal visible to the client page6

--- src/pages/jobs/JobProposalsPage.tsx
+++ src/pages/jobs/JobProposalsPage.tsx
@@ -4,3 +4,23 @@ import { useAuth } from "../../hooks/useAuth";
 import { useJobs } from "../../hooks/useJobs";
 import { useProposals } from "../../context/ProposalsContext";
 
+export default function JobProposalsPage() {
+
+    const { jobId } = useParams();
+    const navigate = useNavigate();
+  
+    const { user } = useAuth();
+    const { jobs } = useJobs();
+    const { getProposalsByJobId } = useProposals();
+  
+    const job = useMemo(() => jobs.find((j) => j.id === jobId), [jobs, jobId]);
+  
+    const proposals = useMemo(() => {
+      if (!jobId) return [];
+      return getProposalsByJobId(jobId);
+    }, [jobId, getProposalsByJobId]);
+
+     
+
+
+}
