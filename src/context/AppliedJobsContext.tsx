3f56554d40e8767f1c1a0f38e1cacf42704f5ad7
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 12:07:25 +0530

    apply job2

--- src/context/AppliedJobsContext.tsx
+++ src/context/AppliedJobsContext.tsx
@@ -1,1 +1,8 @@
 import React, { createContext, useContext, useMemo, useState } from "react";
+
+type AppliedJobsContextType = {
+    appliedJobIds: string[];
+    applyToJob: (jobId: string) => void;
+    isApplied: (jobId: string) => boolean;
+    removeApplication: (jobId: string) => void;
+  };
