1a5ec48aee341214e1fcd7ffbde36c823affa791
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 16:19:52 +0530

    edit job details page

--- src/pages/jobs/JobDetailsPage.tsx
+++ src/pages/jobs/JobDetailsPage.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useJobs } from "../../hooks/useJobs";
 import { useAppliedJobs } from "../../hooks/useAppliedJobs";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function JobDetailsPage() {
 
