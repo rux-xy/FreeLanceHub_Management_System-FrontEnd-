92c54365d26442d938506dd246c21bed212d05bb
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 18:01:12 +0530

    job proposal visible to the client page4

--- src/pages/jobs/JobProposalsPage.tsx
+++ src/pages/jobs/JobProposalsPage.tsx
@@ -1,3 +1,5 @@
 import { useMemo } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useJobs } from "../../hooks/useJobs";
+
