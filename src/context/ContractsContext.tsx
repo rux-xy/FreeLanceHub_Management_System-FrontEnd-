6e9ef749099eb79fafce41fb63d64188d114f108
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 23:12:48 +0530

    create contract page2

--- src/context/ContractsContext.tsx
+++ src/context/ContractsContext.tsx
@@ -1,2 +1,5 @@
 import React, { createContext, useContext, useMemo, useState } from "react";
 import type { Contract } from "../types/contract.types";
+
+const STORAGE_KEY = "freelancehub_contracts";
+
