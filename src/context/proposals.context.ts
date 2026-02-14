61ea08202776c21b979a709b72fadcbbcd026f45
SachinPerera20 <sachinnipun20@gmail.com>
2026-02-14 15:33:30 +0530

    proposal context

---
+++ src/context/proposals.context.ts
@@ -0,0 +1,9 @@
+// src/context/proposals.context.ts
+
+import type { Proposal } from "../types/proposal.types";
+
+export type ProposalsContextValue = {
+  proposals: Proposal[];
+  addProposal: (data: Omit<Proposal, "id" | "createdAt" | "status">) => void;
+  getProposalsByJobId: (jobId: string) => Proposal[];
+};
